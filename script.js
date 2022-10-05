97391fc41b27f1e8047c7d4836fc0ef67458c265
Amanda Zanata <amandazanata46@gmail.com>
2022-10-05 12:34:52 -0300

    cria paleta de cores

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-let palletteColor = document.getElementById('.color-palette');
+const divMaker = document.createElement('div');
+divMaker.id = 'color-palette';
 
-function fazTabela() {
-  const table = document.createElement('table');
-  const tablebd = document.createElement('tbody');
+function generatePalette() {
+  const divPai = document.querySelector('#color-palette');
 
   for (let index = 0; index < 4; index += 1) {
-    const row = document.createElement('tr');
-    for (let index2 = 0; index2 < 1; index2 += 1) {
-      const line = document.createElement('td');
-      const lineText = document.createTextNode(`line in row ${index}, column ${index2}`);
-      line.appendChild(lineText);
-      row.appendChild(line);
-    }
-    tablebd.appendChild(row);
+    const divColors = document.createElement('div');
+    divColors.classList.add('color');
+    divColors.setAttribute('class', 'color', index);
+
+    divPai.appendChild(divColors);
   }
-  table.appendChild(tablebd);
-  document.body.appendChild(table);
-  table.setAttribute('border', '4');
 }
+generatePalette();

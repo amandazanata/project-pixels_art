7a64269705471d30151141812a8b7f08ec8ffa0f
Amanda Zanata <amandazanata46@gmail.com>
2022-10-05 13:37:34 -0300

    adiciona css colors

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const divMaker = document.createElement('div');
-divMaker.id = 'color-palette';
-
 function generatePalette() {
-  const divPai = document.querySelector('#color-palette');
+  const colorPalette = document.querySelector('#color-palette');
 
   for (let index = 0; index < 4; index += 1) {
     const divColors = document.createElement('div');
     divColors.classList.add('color');
-    divColors.setAttribute('class', 'color', index);
+    divColors.setAttribute('class', 'color' + index);
 
-    divPai.appendChild(divColors);
+    colorPalette.appendChild(divColors);
   }
 }
 generatePalette();

0c5b09f0d56fc91a1467bbb80f5a149a383f271e
Amanda Zanata <amandazanata46@gmail.com>
2022-10-06 13:53:07 -0300

    adiciona divs paleta de cores

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-window.onload = function() {
-
-  const capture = document.querySelector('#color-palette');
+window.onload = function () {
+  const capturaElemento = document.getElementById('color-palette');
 
   for (let index = 0; index < 4; index += 1) {
-    const divColors = document.createElement('div');
-    let colorPalette = divColors[index];
-    listColors.innerHTML = colorPalette;
-    divColors.classList.add('color');
-    divColors.id = index;
-    colorPalette.setAttribute('class', 'color', index);
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = index;
+    createDiv.setAttribute('class', 'color' + index);
 
-  colorPalette.appendChild(divColors);
+    capturaElemento.appendChild(createDiv);
   }
 
 /* Termina o escopo do window.onload */
+}

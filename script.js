1b0bd638260e28eb7f05ae0acbf96fc1f6ab8267
Amanda Zanata <amandazanata46@gmail.com>
2022-10-08 11:07:01 -0300

    adiciona funcao random

--- script.js
+++ script.js
@@ -8,23 +8,46 @@ function createTagH1() {
   const h1 = document.createElement('h1');
   h1.innerHTML = 'Paleta de Cores';
   getId.appendChild(h1);
-};
+}
 
 function createPixel() {
   const getIdPalette = document.getElementById('color-palette');
+  getIdPalette.style.display = 'flex';
 
   for (let index = 0; index < 4; index += 1) {
     const createDiv = document.createElement('div');
     createDiv.style.width = '50px';
     createDiv.style.height = '50px';
     createDiv.style.border = 'solid 1px black';
-    createDiv.classList = 'color';
+    createDiv.style.marginLeft = '5px';
+    createDiv.className = 'color';
     createDiv.id = index + 1;
     getIdPalette.appendChild(createDiv);
   }
-  return createDiv;
+  return getIdPalette;
+}
+
+function colorRandom() {
+  const liz = Math.floor(Math.random() * 256);
+  const manu = Math.floor(Math.random() * 256);
+  const pedro = Math.floor(Math.random() * 256);
+  return `rgb(${liz}, ${manu}, ${pedro})`;
+}
+
+const pixelate = () => {
+  const paint = document.getElementsByClassName('color');
+  for (let index = 0; index < paint.length; index += 1) {
+    paint[index].style.backgroundColor = colorRandom();
+  }
 }
 
 /* Chamada das Funções ----------------------------------------------------------------*/
 createTagH1();
 createPixel();
+colorRandom();
+pixelate();
+
+// exemplos de javascript
+//
+// template literals: coloca a variável dentro das chaves >> `rgb(${liz}, ${manu}, ${pedro})`;
+//

7c064d43982b36c15a0f09692b9756205f40f948
Amanda Zanata <amandazanata46@gmail.com>
2022-10-09 22:00:19 -0300

    adiciona selected

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
-/* Variáveis ----------------------------------------------------------------*/
-
-
-/* Funções ----------------------------------------------------------------*/
-
-// Salva cores na localStorage
-window.onload = () => {
-const getColorClass = document.getElementsByClassName('color');
-const colorBoard = [];
-
-function colorify() {
-  for (let index = 0; index < 4; index += 1) {
-    colorBoard[index] = getColorClass[index].style.backgroundColor;
+window.onload = function open() {
+  function createTagH1() {
+    const getHeader = document.querySelector('header');
+    const h1 = document.createElement('h1');
+    h1.id = 'title';
+    h1.innerHTML = 'Paleta de Cores';
+    getHeader.appendChild(h1);
   }
-  localStorage.setItem('colorPalette', JSON.stringify(colorBoard));
-}
-  
-function recolorify() {
-  const colored = JSON.parse(localStorage.getItem('colorPalette'));
-  for (let index = 0; index < 4; index += 1); {
-  getColorClass[index].style.backgroundColor = colored[index];
-  }
-}
-  
-if (localStorage.getItem('colorPalette') === null) {
-  pixelate();
-} else {
-  recolorify();
-}
+  createTagH1();
 
-colorify();
-recolorify();
-
-} // fim window.onload
-
-// Cria h1 e adiciona o título
-
-function createTagH1() {
-  const getHeader = document.querySelector('header');
-  const h1 = document.createElement('h1');
-  h1.id = 'title';
-  h1.innerHTML = 'Paleta de Cores';
-  getHeader.appendChild(h1);
-}
-
-// Coloca primeiro pixel da paleta com a cor preta
-
-const firstColorPalette = () => {
-  const firstColor = document.getElementById('1');
-  firstColor.style.backgroundColor = ('black');
+  const addClassSelected = document.getElementById('black-color');
+  addClassSelected.style.backgroundColor = 'black';
+  addClassSelected.classList.add('selected');
+  console.log(addClassSelected);
 }
 
 // Gerador de cores aleatórias
@@ -68,20 +32,18 @@ const pixelate = () => {
     return pixelate * (256 / colorRandom) % 256;
   }
 }
-colorRandom();
-pixelate();
 
-// Botão de seleção de cores aleatórias
+// Botão de cores aleatórias
 
 const getBtnColors = document.getElementById('button-random-color');
 getBtnColors.innerHTML = 'Cores aleatórias';
 getBtnColors.addEventListener('click', pixelate);
 
 // Cria paleta de cores com 4 pixels
+
 const getIdPalette = document.getElementById('color-palette');
 
-function createPixel() {
-  getIdPalette.style.display = 'flex';
+function createPalette() {
 
   for (let index = 0; index < 4; index += 1) {
     const createDiv = document.createElement('div');
@@ -91,13 +53,16 @@ function createPixel() {
     createDiv.style.marginLeft = '5px';
     createDiv.style.display = 'inline-block';
     createDiv.className = 'color';
-    createDiv.id = index + 1;
     getIdPalette.appendChild(createDiv);
   }
   return getIdPalette;
 }
+createPalette();
+colorRandom();
+pixelate();
 
 // Cria quadro de pixels 5x5
+
 const getIdPixBoard = document.getElementById('pixel-board');
 
 function createBoard(number) {
@@ -116,35 +81,35 @@ function createBoard(number) {
     }
     getIdPixBoard.appendChild(inPixels);
   }
+
 }
-
-/* const clearPixelate = () => {
-  const clearPix = document.getElementsByClassName('color');
-  for (let index = 1; index < paint.length; index += 1) {
-    paint[index].style.backgroundColor = colorRandom();
-  }
-  if (colorRandom <= 255 && colorRandom === 0) {
-    return pixelate * (256 / colorRandom) % 256;
-  }
-}
-
-// Botão para limpar quadro de pixels
-
-/* const getBtnColors = document.getElementById('clear-board');
-getBtnColors.innerHTML = 'Limpar';
-getBtnColors.addEventListener('click', -------); */
-
-/* Chamada das Funções ----------------------------------------------------------------*/
-createTagH1();
-createPixel();
-firstColorPalette();
-colorRandom();
-pixelate();
 createBoard(5);
 
-// termina window.onload
+const getColorClass = document.getElementsByClassName('color');
+const colorBoard = [];
+
+function colorify() {
+  for (let index = 0; index < 4; index += 1) {
+    colorBoard[index] = getColorClass[index].style.backgroundColor;
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(colorBoard));
+}
+colorify();
+
+function recolorify() {
+  const colored = JSON.parse(localStorage.getItem('colorPalette'));
+  for (let index = 0; index < 4; index += 1); {
+  getColorClass[index].style.backgroundColor = colored[index];
+  }
+}
+recolorify();
+
+if (localStorage.getItem('colorPalette') === null) {
+  pixelate();
+} else {
+  recolorify();
+}
 
 // exemplos de javascript
-//
-// template literals: coloca a variável dentro das chaves >> `rgb(${liz}, ${manu}, ${pedro})`;
-//
+
+/* template literals: coloca a variável dentro das chaves >> `rgb(${liz}, ${manu}, ${pedro})`; */

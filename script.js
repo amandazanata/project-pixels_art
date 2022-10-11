038f02969c0d5c506549de0141220f70b9058e65
Amanda Zanata <amandazanata46@gmail.com>
2022-10-11 16:47:56 -0300

    adiciona save colors

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-window.onload = () => {
-  // Guarda paleta de cores gerada, após recarregar página
-  const getColorClass = document.getElementsByClassName('color');
-  const colorBoard = [];
-
-  function colorify() {
-    for (let index = 0; index < 4; index += 1) {
-      colorBoard[index] = getColorClass[index].style.backgroundColor;
-    }
-    localStorage.setItem('colorPalette', JSON.stringify(colorBoard));
-  }
-  colorify();
-
-  if (localStorage.getItem('colorPalette') === null) {
-    pixelate();
-  }
-}
-// Fim window.onload
-
-function createTagH1() {
-  const getHeader = document.querySelector('header');
-  const h1 = document.createElement('h1');
-  h1.id = 'title';
-  h1.innerHTML = 'Paleta de Cores';
-  getHeader.appendChild(h1);
-}
-createTagH1();
-
 // Gerador de cores aleatórias
-
 function colorRandom() {
   const liz = Math.floor(Math.random() * 256);
   const manu = Math.floor(Math.random() * 256);
@@ -35,23 +6,20 @@ function colorRandom() {
   return `rgb(${liz}, ${manu}, ${pedro})`;
 }
 
-let paint = document.getElementsByClassName('color');
-
 const pixelate = () => {
+  const paint = document.getElementsByClassName('color');
   for (let index = 1; index < paint.length; index += 1) {
     paint[index].style.backgroundColor = colorRandom();
   }
   if (colorRandom <= 255 && colorRandom === 0) {
     return pixelate * (256 / colorRandom) % 256;
   }
-}
+};
 
 // Cria paleta de cores com 4 pixels
-
 const getIdPalette = document.getElementById('color-palette');
 
 function createPalette() {
-
   for (let index = 0; index < 4; index += 1) {
     const createDiv = document.createElement('div');
     createDiv.style.width = '50px';
@@ -67,10 +35,13 @@ function createPalette() {
   }
 }
 
-// Cria quadro de pixels 5x5
+// Botão de cores aleatórias
+const getBtnColors = document.getElementById('button-random-color');
+getBtnColors.innerHTML = 'Cores aleatórias';
+getBtnColors.addEventListener('click', pixelate);
 
+// Cria quadro de pixels 5x5
 const getIdPixBoard = document.getElementById('pixel-board');
-getIdPixBoard.style.marginTop = '5px';
 
 function createBoard(number) {
   for (let index = 0; index < number; index += 1) {
@@ -84,19 +55,18 @@ function createBoard(number) {
       clPixels.style.height = '40px';
       clPixels.style.display = 'inline-block';
       clPixels.style.marginLeft = '5px';
-      clPixels.style.backgroundColor = 'white';
     }
     getIdPixBoard.appendChild(inPixels);
   }
 }
 
-// Funções event bubbling
-
+// Função cor preta no quadro
 function startBlack() {
   const blackBoard = document.getElementsByClassName('color')[0];
   blackBoard.classList.add('selected');
 }
 
+// Remove e adiciona o selected
 function selectColor() {
   const selectPalette = document.getElementById('color-palette');
   selectPalette.addEventListener('click', (pick) => {
@@ -106,58 +76,43 @@ function selectColor() {
   });
 }
 
-/* function pickPixelColor(event) {
-  const pixelColor = document.getElementsByClassName('color');
-  pixelColor.target.backgroundColor('paint[index]');
-} */
-
+// Pinta o quadro
 function pixelateBoard() {
   const getPixelate = document.getElementById('pixel-board');
-  getPixelate.addEventListener('click', (pbo) => {
-    pbo.target.style.backgroundColor = document.getElementsByClassName('selected');
+  getPixelate.addEventListener('click', (pick2) => {
+    pick2.target.style.backgroundColor = document.getElementsByClassName('selected').style.backgroundColor;
   });
 }
 
-function recolorify() {
-  let getColorClass = document.getElementsByClassName('color');
-  const colored = JSON.parse(localStorage.getItem('colorPalette'));
-  for (let index = 0; index < 4; index += 1) {
-    getColorClass += colored[index];
+// Guarda paleta de cores gerada após recarregar página
+const saveColors = () => {
+  const getColorClass = document.getElementsByClassName('color');
+  const colorSave = [];
+  for (let index = 1; index < 4; index += 1) {
+    colorSave.push(getColorClass[index].style.backgroundColor);
   }
-}
+  localStorage.setItem('colorPalette', JSON.stringify(saveColors));
+};
 
-const getBtnColors = document.getElementById('button-random-color');
-getBtnColors.innerHTML = 'Cores aleatórias';
-getBtnColors.addEventListener('click', pixelate);
-getBtnColors.style.zIndex = 1;
-
-window.addEventListener('click', startBlack, true);
-window.addEventListener('click', selectColor, true);
-window.addEventListener('click', createBoard, true);
-window.addEventListener('click', pixelateBoard, true);
-
-// Função e botão limpa quadro de pixels
-
+// Função limpa quadro de pixels
 const clearBoard = () => {
   const turnWhite = document.getElementsByClassName('pixel');
   for (let index = 0; index < turnWhite.length; index += 1) {
-    turnWhite[index].addEventListener('click', true).style.backgroundColor = 'white';
+    turnWhite[index].style.backgroundColor = 'white';
   }
-}
+};
 
+// Botão limpa quadro de pixels
 const getBtnClear = document.getElementById('clear-board');
 getBtnClear.addEventListener('click', clearBoard);
-getBtnClear.innerHTML = 'Limpar';
-getBtnClear.style.marginLeft = '150px';
-getBtnClear.style.zIndex = 2;
-getBtnClear.style.marginBottom = '10px';
 
+// Chama função
 createPalette();
 colorRandom();
 pixelate();
 createBoard(5);
 startBlack();
+clearBoard();
 selectColor();
 pixelateBoard();
-recolorify();
-clearBoard();
+saveColors();
